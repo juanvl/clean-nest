ef7394b20f0e7588dc88a1ebab580bb96300fd04
Juan Victor Leite <juanvictorleite@icloud.com>
2025-10-30 11:39:13 -0300

    feat: add create account controller

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,4 +7,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3333);
 }
+
 bootstrap();
