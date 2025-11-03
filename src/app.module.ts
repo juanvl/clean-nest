c4662ff3fedc0a899ff8e01ea4288c3ca01fae65
Juan Victor Leite <juanvictorleite@icloud.com>
2025-11-03 15:33:45 -0300

    feat: add fetch recent questions controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,7 @@ import { AuthModule } from './auth/auth.module';
 import { AuthenticateController } from './controllers/authenticate.controller';
 import { CreateAccountController } from './controllers/create-account.controller';
 import { CreateQuestionController } from './controllers/create-question.controller';
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller';
 import { envSchema } from './env';
 import { PrismaService } from './prisma/prisma.service';
 
@@ -54,6 +55,7 @@ class HttpExceptionFilter extends BaseExceptionFilter {
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
+    FetchRecentQuestionsController,
   ],
   providers: [
     PrismaService,
