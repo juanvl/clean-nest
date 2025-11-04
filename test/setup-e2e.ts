f60f28cbea0ed45e196a73ec98bc5e2964adb114
Juan Victor Leite <juanvictorleite@icloud.com>
2025-11-04 12:21:30 -0300

    chore: add e2e tests setup

---
+++ test/setup-e2e.ts
@@ -0,0 +1,39 @@
+import 'dotenv/config';
+
+import { execSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
+import { PrismaClient } from '../generated/prisma';
+
+let prisma: PrismaClient;
+
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provider a DATABASE_URL environment variable');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set('schema', schemaId);
+
+  return url.toString();
+}
+
+const schemaId = randomUUID();
+
+beforeAll(() => {
+  const databaseURL = generateUniqueDatabaseURL(schemaId);
+
+  process.env.DATABASE_URL = databaseURL;
+
+  prisma = new PrismaClient();
+
+  execSync('pnpm prisma migrate deploy', {
+    env: { ...process.env, DATABASE_URL: databaseURL },
+    stdio: 'ignore',
+  });
+});
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId}" CASCADE`);
+  await prisma.$disconnect();
+});
